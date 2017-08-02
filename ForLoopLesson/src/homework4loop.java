c12a7f177d72f03b67017bad36e4d55d7db9940d
Nikhil Mantha <NikhilMantha@Nikhils-MacBook-Pro.local>
2017-08-01 22:47:41 -0700

    session 4 java and homework

---
+++ ForLoopLesson/src/homework4loop.java
@@ -0,0 +1,16 @@
+
+public class homework4loop {
+
+	public static void main(String[] args) {
+		
+		for(int i = 5; i<100 ; i = i+10) {
+			if(i < 90) {
+				System.out.print(i + ",");
+			} else {
+				System.out.print(i);
+			}
+		}
+		
+	}
+
+}
