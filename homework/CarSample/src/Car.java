629dd6dd76fbe938d6e57571ee9f69f46ad0d50e
Nikhil Mantha <NikhilMantha@Nikhils-MacBook-Pro.local>
2017-09-06 18:31:53 -0700

    modified

--- homework/CarSample/src/Car.java
+++ homework/CarSample/src/Car.java
@@ -1,10 +1,26 @@
-
 public class Car {
+	String name;
+	int horsePower;
+	boolean started;
+
 	void start() {
-		System.out.println("starting the car");
+		if (started) {
+			System.out.println("Sorry you have started already. you need to stop before starting again.");
+		} else {
+			System.out.println("starting : " + name + " which has:" + horsePower + " horse power");
+			started = true;
+		}
+
 	}
-	
+
 	void stop() {
-		System.out.println("stopping the car");
+		if (started) {
+			System.out.println("stopping: " + name);
+			started = false;
+		} else {
+			System.out.println("sorry,  you have not started yet. please start the car before stopping");
+		}
+        
+		
 	}
 }
